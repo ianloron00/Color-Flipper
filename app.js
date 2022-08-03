c3373b8c9895d3b372e749b9ba6582be4486bdc1
Ian Loron de Almeida <i198933@dac.unicamp.br>
2022-08-03 14:41:47 -0300

    functionality implemented

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+"use strict";
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const hero_btn = document.querySelector("#btn");
+const color_name = document.getElementById("color-page-name");
+
+hero_btn.addEventListener("click", function() {
+        const new_color = getRandomColor();
+        document.body.style.backgroundColor = new_color;
+        color_name.innerHTML = new_color;        
+    } 
+);
+
+function getRandomColor() {
+    const randomId = Math.floor( Math.random() * colors.length );
+    return colors[randomId];
+}
--- hex.js
+++ hex.js
@@ -1,1 +1,21 @@
+"use strict";
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const colorName = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+        const newColor = getRandomColor();
+        document.body.style.backgroundColor = newColor;
+        colorName.innerHTML = newColor;
+    }
+);
+
+function getRandomColor() {
+    let newColor = "#";
+    for (let i = 0; i < 6; i++) {
+        const id = Math.floor( Math.random() * hex.length );
+        newColor += hex[id].toString();
+    }
+
+    return newColor;
+}
